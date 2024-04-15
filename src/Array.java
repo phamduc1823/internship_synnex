554379e8b48233c03058cb327769fe2ffa236a02
phamduc1823 <phamduc1823@gmail.com>
2024-04-15 17:14:05 +0700

    Sap xep chen

--- src/Array.java
+++ src/Array.java
@@ -10,9 +10,27 @@ public class Array {
 //        addArray(arr, 5);
 //        removeArray(arr, 3);
 //        bubbleSort(arr);
-        selectionSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
     }
 
+    //Sắp xếp chèn
+    public static void insertionSort(Integer[] arr) {
+        int arrLength = arr.length;
+        for(int i = 1; i < arrLength; i++){
+            int key = arr[i];
+            int j = i - 1;
+
+            // Di chuyển các phần tử của arr [0 ... i - 1], lớn hơn key
+            // đến một vị trí trước vị trí hiện tại của chúng
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
     //Sắp xếp chọn
     public static void selectionSort(Integer[] arr) {
