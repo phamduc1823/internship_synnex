b23ca6d38f441ff302e843ece0b830a682eb8481
phamduc1823 <phamduc1823@gmail.com>
2024-05-04 14:27:52 +0700

    TCP

--- src/advance/socket/tcp/Server.java
+++ src/advance/socket/tcp/Server.java
@@ -3,16 +3,33 @@ package advance.socket.tcp;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(2211);
-        System.out.println("Waiting for client connection...");
+        Object lock = new Object();
+        Queue<Socket> queue = new LinkedList<>();
+
+        ServerThread serverThread = new ServerThread(serverSocket, lock, queue);
+        serverThread.start();
+
+        System.out.println("Server is active!");
+
         while (true) {
-            //Chap nhan ket noi cua client
-            Socket socket = serverSocket.accept();
-            new ServerProcess(socket).start();
-            System.out.println("Client " + socket.getInetAddress() + " connected!");
+            synchronized (lock) {
+                while (queue.isEmpty()) {
+                    try {
+                        lock.wait();
+                        System.out.println("Queue is empty! wait client connect ...");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Socket socket = queue.poll();
+                new ServerProcess(socket).start();
+            }
         }
     }
 }
